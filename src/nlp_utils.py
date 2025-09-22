588b4427a4017b8404868ada63d8822852e6fa85
AnnaKholkina <delaannushki@gmail.com>
2025-09-22 17:46:34 +0300

    Заглушки + тесты

---
+++ src/nlp_utils.py
@@ -0,0 +1,8 @@
+def tokenize(text: str):
+    """Разбивает текст на слова, приводя к нижнему регистру"""
+    pass
+
+
+def word_frequencies(text: str):
+    """Возвращает частотный словарь слов"""
+    pass
